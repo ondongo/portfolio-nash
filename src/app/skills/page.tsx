49e91825bc853fc7d3e48fb5d4877d5bf06c3474
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2024-10-26 07:47:03 +0200

    Starting dev skills page

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { AnimatedPinDemo } from "@/components/AnimatePinDemo";
+
+import React from "react";
 
 function page() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0 ">
+      <div className="container mx-auto">
+        <div className="flex flex-row justify-between">
+          <div className="flex-1 flex">Section tab </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-[20px]">
+            <AnimatedPinDemo />
+            <AnimatedPinDemo />
+            <AnimatedPinDemo />
+            <AnimatedPinDemo />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default page
+export default page;
--- src/components/CardStack.tsx
+++ src/components/CardStack.tsx
@@ -52,7 +52,7 @@ export const CardStack = ({
         return (
           <motion.div
             key={card.id}
-            className="absolute  bg-slate-900 h-60 w-60 md:h-60 md:w-[500px] rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-white/[0.1]  shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between"
+            className="absolute  bg-slate-900 h-60 w-60 md:h-60 md:w-[500px] rounded-3xl p-4 shadow-xl border border-neutral-200   shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between"
             style={{
               transformOrigin: "top center",
             }}
