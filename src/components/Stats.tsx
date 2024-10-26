13bd9a4d724bf61832644021a855ce327425f36a
Gloire__ODG <93401301+ondongo@users.noreply.github.com>
2024-10-26 03:53:53 +0200

    Update Stats.tsx

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -28,17 +28,23 @@ function Stats() {
       <div className="container mx-auto">
         <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none ">
           {stats.map((item: any, index: any) => {
+            const shouldDisplayPlus = item.num > 10;
             return (
               <div
                 className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
                 key={index}
               >
-                <CountUp
-                  end={item.num}
-                  duration={5}
-                  delay={2}
-                  className="text-4xl xl:text-6xl font-extrabold"
-                />
+                <p className="text-4xl xl:text-6xl font-extrabold flex items-center">
+                  {shouldDisplayPlus && (
+                    <span className="transition-opacity duration-500"> +&nbsp;</span>
+                  )}
+                  <CountUp
+                    end={item.num}
+                    duration={5}
+                    delay={2}
+                    className="inline"
+                  />
+                </p>
                 <p
                   className={`${
                     item.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
